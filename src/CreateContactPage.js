87f13cefcebf4239b15360b4e089f29ca9f3d710
saadman2001rahman <saadman2001rahman@gmail.com>
2024-02-01 14:09:24 -0700

    validation works for create page

--- src/CreateContactPage.js
+++ src/CreateContactPage.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 const CreateContactPage = ({ onAddContact, onCancelClick }) => {
     const [contact, setContact] = useState({
@@ -7,26 +7,62 @@ const CreateContactPage = ({ onAddContact, onCancelClick }) => {
         lastName: '',
     });
 
+    const [errors, setErrors] = useState({});
+    const [submitting, setSubmitting] = useState(false);
+
     const handleInputChange = (e) => {
         const { name, value } = e.target;
         setContact({ ...contact, [name]: value });
     };
 
-    const handleSubmit = () => {
-        onAddContact(contact);
-        setContact({
-            email: '',
-            firstName: '',
-            lastName: '',
-        });
+
+    const validateValues = (contact) => {
+        let errors = {};
+        if (contact.email.trim() === '') {
+            errors.email = "Email cannot be empty";
+        }
+        if (contact.firstName.trim() === '') {
+            errors.firstName = "First Name cannot be empty";
+        }
+
+        return errors;
     };
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        setErrors(validateValues(contact));
+        setSubmitting(true);
+    };
+
+    const finishSubmit = () => {
+        console.log(contact);
+        onAddContact(contact);
+    };
+    useEffect(() => {
+        if (Object.keys(errors).length === 0 && submitting) {
+            finishSubmit();
+        }
+    }, [errors]);
+
 
     return (
         <div>
+            {Object.keys(errors).length === 0 && submitting ? (
+                <span className="success">Successfully submitted ✓</span>
+            ) : null}
             <h2>Create Contact</h2>
             <form>
                 <label>Email:</label>
-                <input type="email" name="email" value={contact.email} onChange={handleInputChange} required />
+                <input
+                    type="email"
+                    name="email"
+                    value={contact.email}
+                    onChange={handleInputChange}
+                    style={{ border: errors.email ? "1px solid red" : null }}
+                />
+                {errors.email ? (
+                    <p className="error">Email should not be empty</p>
+                ) : null}
 
                 <label>First Name:</label>
                 <input
@@ -34,10 +70,11 @@ const CreateContactPage = ({ onAddContact, onCancelClick }) => {
                     name="firstName"
                     value={contact.firstName}
                     onChange={handleInputChange}
-                    minLength={3}
-                    maxLength={25}
-                    required
+                    style={{ borderColor: errors.firstName ? 'red' : '' }}
                 />
+                {errors.firstName ? (
+                    <p className="error">First Name should not be empty</p>
+                ) : null}
 
                 <label>Last Name (Optional):</label>
                 <input
