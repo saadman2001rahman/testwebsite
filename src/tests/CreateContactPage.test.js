0803a53fffa6476ced2426fa1af605e4b2341457
saadman2001rahman <saadman2001rahman@gmail.com>
2024-02-08 16:22:34 -0700

    added more unit testing for input validations

--- src/tests/CreateContactPage.test.js
+++ src/tests/CreateContactPage.test.js
@@ -1,4 +1,3 @@
-// CreateContactPage.test.js
 import React from 'react';
 import { render, screen, fireEvent, waitFor } from '@testing-library/react';
 import CreateContactPage from '../components/CreateContactPage';
@@ -6,7 +5,14 @@ import CreateContactPage from '../components/CreateContactPage';
 const mockOnAddContact = jest.fn();
 const mockOnCancelClick = jest.fn();
 
+const mockContactsList = [
+    { firstName: 'Saadman', lastName: 'Rahman', email: 'sdmn@example.com' },
+    { firstName: 'Rahman', lastName: 'Saadman', email: 'rhmn@example.com' },
+];
+
+
 describe('CreateContactPage Component', () => {
+    //check if the website renders correctly
     it('renders the form correctly', () => {
         render(<CreateContactPage contactsList={[]} onAddContact={mockOnAddContact} onCancelClick={mockOnCancelClick} />);
 
@@ -18,6 +24,7 @@ describe('CreateContactPage Component', () => {
         expect(screen.getByText('Cancel')).toBeInTheDocument();
     });
 
+    //check if website renders inputs
     it('handles input changes correctly', () => {
         render(<CreateContactPage contactsList={[]} onAddContact={mockOnAddContact} onCancelClick={mockOnCancelClick} />);
 
@@ -30,23 +37,46 @@ describe('CreateContactPage Component', () => {
         expect(screen.getByLabelText('Last Name (Optional):')).toHaveValue('Doe');
     });
 
+    //check if we can create a contact with a valid input (first and last name)
     it('calls onAddContact when "Create Contact" button is clicked with valid input', async () => {
         render(<CreateContactPage contactsList={[]} onAddContact={mockOnAddContact} onCancelClick={mockOnCancelClick} />);
 
         fireEvent.change(screen.getByLabelText('Email:'), { target: { value: 'test@example.com' } });
         fireEvent.change(screen.getByLabelText('First Name:'), { target: { value: 'John' } });
+        fireEvent.change(screen.getByLabelText('Last Name (Optional):'), { target: { value: 'Rahm' } });
 
-        fireEvent.click(screen.getByText('Create Contact'));
+
+        fireEvent.click(screen.getByText('Create'));
 
         await waitFor(() => {
             expect(mockOnAddContact).toHaveBeenCalledWith({
                 email: 'test@example.com',
                 firstName: 'John',
-                // lastName: '',
+                lastName: 'Rahm',
             });
         });
     });
 
+    //check if we can create a contact with a valid input (first name only)
+    it('calls onAddContact when "Create Contact" button is clicked with valid input, no last name entered', async () => {
+        render(<CreateContactPage contactsList={[]} onAddContact={mockOnAddContact} onCancelClick={mockOnCancelClick} />);
+
+        fireEvent.change(screen.getByLabelText('Email:'), { target: { value: 'test@example.com' } });
+        fireEvent.change(screen.getByLabelText('First Name:'), { target: { value: 'John' } });
+
+
+        fireEvent.click(screen.getByText('Create'));
+
+        await waitFor(() => {
+            expect(mockOnAddContact).toHaveBeenCalledWith({
+                email: 'test@example.com',
+                firstName: 'John',
+                lastName: ''
+            });
+        });
+    });
+
+    //check if we can cancel a creation of a contact
     it('calls onCancelClick when "Cancel" button is clicked', () => {
         render(<CreateContactPage contactsList={[]} onAddContact={mockOnAddContact} onCancelClick={mockOnCancelClick} />);
 
@@ -54,4 +84,83 @@ describe('CreateContactPage Component', () => {
 
         expect(mockOnCancelClick).toHaveBeenCalled();
     });
+
+    //test for input validations
+
+    it('Gives the correct error message when first name is too short', async () => {
+        render(<CreateContactPage contactsList={[]} onAddContact={mockOnAddContact} onCancelClick={mockOnCancelClick} />);
+
+        fireEvent.change(screen.getByLabelText('Email:'), { target: { value: 'test1@example.com' } });
+        fireEvent.change(screen.getByLabelText('First Name:'), { target: { value: '' } });
+
+        fireEvent.click(screen.getByText('Create'));
+        await waitFor(() => {
+            expect(screen.getByText('First Name too short')).toBeInTheDocument();
+        });
+    });
+
+    it('Gives the correct error message when first name is too big', async () => {
+        render(<CreateContactPage contactsList={[]} onAddContact={mockOnAddContact} onCancelClick={mockOnCancelClick} />);
+
+        fireEvent.change(screen.getByLabelText('Email:'), { target: { value: 'test1@example.com' } });
+        fireEvent.change(screen.getByLabelText('First Name:'), { target: { value: 'bbbbbbbbbbbbbbbbbbbbbbbbbb' } });
+
+        fireEvent.click(screen.getByText('Create'));
+        await waitFor(() => {
+            expect(screen.getByText('First Name too long')).toBeInTheDocument();
+        });
+    });
+
+    it('Gives the correct error message when last name is too short', async () => {
+        render(<CreateContactPage contactsList={[]} onAddContact={mockOnAddContact} onCancelClick={mockOnCancelClick} />);
+
+        fireEvent.change(screen.getByLabelText('Email:'), { target: { value: 'test1@example.com' } });
+        fireEvent.change(screen.getByLabelText('First Name:'), { target: { value: 'ssss' } });
+        fireEvent.change(screen.getByLabelText('Last Name (Optional):'), { target: { value: 's' } });
+
+        fireEvent.click(screen.getByText('Create'));
+        await waitFor(() => {
+            expect(screen.getByText('Last name has to be between 2 and 30 characters')).toBeInTheDocument();
+        });
+    });
+
+    it('Gives the correct error message when last name is too big', async () => {
+        render(<CreateContactPage contactsList={[]} onAddContact={mockOnAddContact} onCancelClick={mockOnCancelClick} />);
+
+        fireEvent.change(screen.getByLabelText('Email:'), { target: { value: 'test1@example.com' } });
+        fireEvent.change(screen.getByLabelText('First Name:'), { target: { value: 'bbbbbbbbbbbb' } });
+        fireEvent.change(screen.getByLabelText('Last Name (Optional):'), { target: { value: 'bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb' } });
+
+
+        fireEvent.click(screen.getByText('Create'));
+        await waitFor(() => {
+            expect(screen.getByText('Last name has to be between 2 and 30 characters')).toBeInTheDocument();
+        });
+    });
+
+    it('Gives the correct error message when email is empty', async () => {
+        render(<CreateContactPage contactsList={[]} onAddContact={mockOnAddContact} onCancelClick={mockOnCancelClick} />);
+
+        fireEvent.change(screen.getByLabelText('First Name:'), { target: { value: 'bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb' } });
+
+        fireEvent.click(screen.getByText('Create'));
+        await waitFor(() => {
+            expect(screen.getByText('Email cannot be empty')).toBeInTheDocument();
+        });
+    });
+
+
+    it('Gives the correct error message when email is not unique', async () => {
+        render(<CreateContactPage contactsList={mockContactsList} onAddContact={mockOnAddContact} onCancelClick={mockOnCancelClick} />);
+
+        fireEvent.change(screen.getByLabelText('Email:'), { target: { value: 'sdmn@example.com' } });
+        fireEvent.change(screen.getByLabelText('First Name:'), { target: { value: 'bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb' } });
+
+        fireEvent.click(screen.getByText('Create'));
+        await waitFor(() => {
+            expect(screen.getByText('Email must be unique')).toBeInTheDocument();
+        });
+    });
+
+
 });
--- src/tests/EditContactPage.test.js
+++ src/tests/EditContactPage.test.js
@@ -1,6 +1,6 @@
 // EditContactPage.test.js
 import React from 'react';
-import { render, screen, fireEvent } from '@testing-library/react';
+import { render, screen, fireEvent, waitFor } from '@testing-library/react';
 import EditContactPage from '../components/EditContactPage';
 
 const mockContact = {
@@ -13,6 +13,7 @@ const mockOnUpdateContact = jest.fn();
 const mockOnCancelClick = jest.fn();
 
 describe('EditContactPage Component', () => {
+    //check if website renders properly
     it('renders form elements correctly', () => {
         render(<EditContactPage contact={mockContact} onUpdateContact={mockOnUpdateContact} onCancelClick={mockOnCancelClick} />);
 
@@ -23,19 +24,44 @@ describe('EditContactPage Component', () => {
         expect(screen.getByText('Cancel')).toBeInTheDocument();
     });
 
-    // it('calls the correct function when "Save Changes" button is clicked', () => {
-    //     render(<EditContactPage contact={mockContact} onUpdateContact={mockOnUpdateContact} onCancelClick={mockOnCancelClick} />);
+    //check valid input when only first name is edited, both names will go and first name will be changed
+    it('calls the correct function when "Save Changes" button is clicked, first name on;y', async () => {
+        render(<EditContactPage contact={mockContact} onUpdateContact={mockOnUpdateContact} onCancelClick={mockOnCancelClick} />);
 
-    //     fireEvent.change(screen.getByLabelText('First Name:'), { target: { value: 'UpdatedName' } });
-    //     fireEvent.click(screen.getByText('Save Changes'));
+        fireEvent.change(screen.getByLabelText('First Name:'), { target: { value: 'UpdatedName' } });
+        fireEvent.click(screen.getByText('Save Changes'));
 
-    //     // Check if the onUpdateContact function is called with the correct edited contact data
-    //     expect(mockOnUpdateContact).toHaveBeenCalledWith({
-    //         email: 'test@example.com',
-    //         firstName: 'UpdatedName',
-    //         lastName: 'Doe',
-    //     });
-    // });
+        // Check if the onUpdateContact function is called with the correct edited contact data
+        await waitFor(() => {
+            expect(mockOnUpdateContact).toHaveBeenCalledWith({
+                email: 'test@example.com',
+                firstName: 'UpdatedName',
+                lastName: '',
+            });
+        });
+
+    });
+
+    //check valid input when both first and last name is edited, both names will be changed
+    it('calls the correct function when "Save Changes" button is clicked, both names', async () => {
+        render(<EditContactPage contact={mockContact} onUpdateContact={mockOnUpdateContact} onCancelClick={mockOnCancelClick} />);
+
+        fireEvent.change(screen.getByLabelText('First Name:'), { target: { value: 'UpdatedName' } });
+        fireEvent.change(screen.getByLabelText('Last Name (Optional):'), { target: { value: 'UpdatedLastName' } });
+
+        fireEvent.click(screen.getByText('Save Changes'));
+
+        // Check if the onUpdateContact function is called with the correct edited contact data
+        await waitFor(() => {
+            expect(mockOnUpdateContact).toHaveBeenCalledWith({
+                email: 'test@example.com',
+                firstName: 'UpdatedName',
+                lastName: 'UpdatedLastName',
+            });
+        });
+
+    });
+
 
     it('calls the correct function when "Cancel" button is clicked', () => {
         render(<EditContactPage contact={mockContact} onUpdateContact={mockOnUpdateContact} onCancelClick={mockOnCancelClick} />);
@@ -44,5 +70,58 @@ describe('EditContactPage Component', () => {
 
         expect(mockOnCancelClick).toHaveBeenCalled();
     });
+
+
+    //test for input validations
+
+    it('Gives the correct error message when first name is too short', async () => {
+        render(<EditContactPage contact={mockContact} onUpdateContact={mockOnUpdateContact} onCancelClick={mockOnCancelClick} />);
+
+        fireEvent.change(screen.getByLabelText('First Name:'), { target: { value: 'ss' } });
+        fireEvent.click(screen.getByText('Save Changes'));
+
+        await waitFor(() => {
+            expect(screen.getByText('First Name too short')).toBeInTheDocument();
+        });
+
+    });
+
+    //check ivalid input when only first name is edited, first name error will be displayed
+    it('Gives the correct message when first name is too big', async () => {
+        render(<EditContactPage contact={mockContact} onUpdateContact={mockOnUpdateContact} onCancelClick={mockOnCancelClick} />);
+
+        fireEvent.change(screen.getByLabelText('First Name:'), { target: { value: 'bbbbbbbbbbbbbbbbbbbbbbbbbb' } });
+        fireEvent.click(screen.getByText('Save Changes'));
+
+        await waitFor(() => {
+            expect(screen.getByText('First Name too long')).toBeInTheDocument();
+        });
+    });
+
+    //check ivalid input when only last name is edited,
+    it('Gives the correct error message when last name is too short', async () => {
+        render(<EditContactPage contact={mockContact} onUpdateContact={mockOnUpdateContact} onCancelClick={mockOnCancelClick} />);
+
+        fireEvent.change(screen.getByLabelText('Last Name (Optional):'), { target: { value: 's' } });
+        fireEvent.click(screen.getByText('Save Changes'));
+
+        await waitFor(() => {
+            expect(screen.getByText('Last name has to be between 2 and 30 characters')).toBeInTheDocument();
+        });
+
+    });
+
+    //check ivalid input when only last name is edited
+    it('Gives the correct message when last name is too big', async () => {
+        render(<EditContactPage contact={mockContact} onUpdateContact={mockOnUpdateContact} onCancelClick={mockOnCancelClick} />);
+
+        fireEvent.change(screen.getByLabelText('Last Name (Optional):'), { target: { value: 'bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb' } });
+        fireEvent.click(screen.getByText('Save Changes'));
+
+        await waitFor(() => {
+            expect(screen.getByText('Last name has to be between 2 and 30 characters')).toBeInTheDocument();
+        });
+    });
+
 
 });
